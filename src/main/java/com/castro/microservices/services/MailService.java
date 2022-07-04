be0e8c74f5f64b550134b91ece115eaa6a34ea63
Diegocastro21 <Diegocamilocastrooliveros@gmail.com>
2022-07-03 23:38:01 -0500

    Subida

---
+++ src/main/java/com/castro/microservices/services/MailService.java
@@ -0,0 +1,25 @@
+package com.castro.microservices.services;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String text){
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom("colegio.castro.test@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+
+        mailSender.send(message);
+
+    }
+
+}
